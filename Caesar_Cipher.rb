46ea9a766d1eb53e8ebdcffcf178b2fc7591a975
David <berghdavid@hotmail.com>
2020-10-31 16:25:14 +0100

    Done

---
+++ Caesar_Cipher.rb
@@ -0,0 +1,22 @@
+def caesar_cipher(string = "What a string!", shift = 5)
+    upcase_first = "A".ord
+    lowcase_first = "a".ord
+    chars = "z".ord- "a".ord + 1
+    caesar_string = ""
+
+    string.each_char do |char|
+        if char =~ /[a-z]/
+            shifted = (((char.ord - lowcase_first + shift) % chars) + lowcase_first).chr
+            caesar_string += shifted
+        elsif char =~ /[A-Z]/
+            shifted = (((char.ord - upcase_first + shift) % chars) + upcase_first).chr
+            caesar_string += shifted
+        else
+            caesar_string += char
+        end
+    end
+
+    return caesar_string
+end
+    
+puts caesar_cipher
