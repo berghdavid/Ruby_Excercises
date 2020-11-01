ea953b3101fa1addb073ff4d8fa3c3eaf1e4acbd
David <berghdavid@hotmail.com>
2020-11-01 18:59:18 +0100

    used scan. Done

--- Sub_Strings.rb
+++ Sub_Strings.rb
@@ -1,3 +1,13 @@
 def substrings(string, dictionary)
-    
+    matches = Hash.new(0)
+    downcased = string.downcase
+
+    dictionary.each do |dict_word|
+        matches[dict_word] += downcased.scan(dict_word.downcase).length
+    end
+    return matches
 end
+
+dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+string = "Howdy partner, sit down! How's it going?"
+puts substrings(string, dictionary)
