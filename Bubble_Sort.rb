11d024673424528d873b0625cf3a7e1e3235752e
David <berghdavid@hotmail.com>
2020-11-01 20:32:11 +0100

    Done

---
+++ Bubble_Sort.rb
@@ -0,0 +1,17 @@
+def bubble_sort(arr)
+    index_popped = (0..arr.length-2)
+    done = false
+
+    until done do
+        done = true
+        index_popped.each do |index|
+            if(arr[index] > arr[index + 1])
+                arr[index], arr[index + 1] = arr[index + 1], arr[index]
+                done = false
+            end
+        end
+    end
+    return arr
+end
+
+puts bubble_sort([4,3,78,2,0,2])
