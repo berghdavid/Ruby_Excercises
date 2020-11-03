7804ddeb2c7017c4c7b797ba4b19215b2e9d085e
David <berghdavid@hotmail.com>
2020-11-03 14:50:36 +0100

    promptRounds done

--- Mastermind.rb
+++ Mastermind.rb
@@ -1,8 +1,24 @@
-class mastermind
+class Mastermind
     attr_reader :code
+
     def initialize
         @available_codes = {0 => "Red", 1 => "Blue", 2 => "Green", 3 => "White"}
         generateCode
+        puts "Code generated..."
+    end
+
+    private def promptRounds
+        while (true)
+            puts "How many rounds do you want to play?"
+            input = gets.chomp.strip
+            input = Integer(gets.chomp.strip) rescue false
+            if(input != 0)
+                @rounds = input
+                break;
+            else
+                puts "Invalid input"
+            end
+        end
     end
 
     private def generateCode
@@ -11,5 +27,5 @@ class mastermind
     end
 end
 
-game = mastermind.new()
+game = Mastermind.new
 puts game.code
