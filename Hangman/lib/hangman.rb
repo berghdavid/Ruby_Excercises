d34e7f0e6007a5a796994f7771fbbb778335bf69
David <berghdavid@hotmail.com>
2020-11-10 22:42:14 +0100

    Done

--- Hangman/lib/hangman.rb
+++ Hangman/lib/hangman.rb
@@ -6,7 +6,8 @@ class Hangman
     def initialize()
         @attempts_left = 8
         @game_over = false
-        @wrongly_guessed = []
+        @guessed_word = ""
+        @wrongly_guessed = ""
     end
 
     def generate_word()
@@ -17,13 +18,12 @@ class Hangman
             @word = nil
             while(@word == nil)
                 nr = rand(lines.length)
-                new_word = lines[nr]
+                new_word = lines[nr].chomp
                 if(new_word.length >= 5)
                     @word = new_word.capitalize!
                 end
             end
 
-            @guessed_word = ""
             @word.length.times { @guessed_word += "_"}
 
             if(@word != nil)
@@ -46,16 +46,50 @@ class Hangman
     end
 
     private def prompt_guesses
-        output_guess = @guessed_word.gsub(/(.{1})(?=.)/, '\1 \2')
-
         while(!@game_over)
+            output_guess = @guessed_word.gsub(/(.{1})(?=.)/, '\1 \2')
             puts "========================================================"
             puts "You have #{@attempts_left} attempts left."
-            puts "You have guessed #{output_guess} so far."
-            puts "Your wrongly guessed letters are #{wrongly_guessed}"
-            puts "Guess a new letter..."
+            puts "You have guessed #{output_guess} right so far."
+            puts "Your wrongly guessed letters are #{@wrongly_guessed}"
+            puts "Guess a new letter, or type save to save this game"
             input = gets.chomp.downcase
-            guess_char(input)
+            
+            if(is_char?(input))
+                update_guess(input)
+            elsif(input == "save")
+                save_game
+                break
+            else
+                puts "Input is not a letter, try again."
+            end
+        end
+    end
+
+    public def is_char?(char)
+        char.upcase != char.downcase && char.length == 1
+    end
+
+    private def save_game
+        list_of_names = []
+        contents = CSV.open "saved_games.csv", headers: true, header_converters: :symbol
+        contents.each do |row|
+            list_of_names += [row[:name]]
+        end
+
+        while(true)
+            puts "Enter the name of this game"
+            name = gets.chomp
+            
+            if(!list_of_names.include?(name) && name != "")
+                data = [name, @word, @guessed_word, @attempts_left, @wrongly_guessed]
+                CSV.open('saved_games.csv', 'a') do |row|
+                    row << data
+                end
+                break
+            else
+                puts "Game-name occupied or invalid, try another one"
+            end
         end
     end
 
@@ -65,36 +99,49 @@ class Hangman
         #Puts list of all games
         list_of_names = []
         contents = CSV.open "saved_games.csv", headers: true, header_converters: :symbol
-        puts "Which of the following games do you want to load?"
-
+        
         contents.each do |row|
             name = row[:name]
-            attempts_left = columns[:attempts_left]
+            attempts_left = row[:attempts_left]
+            guessed_word = row[:guessed_word]
+
             list_of_names += [name]
-            puts "Name: #{name}, with #{attempts_left} attempts left."
+            
+            puts "Name: #{name}, completed #{guessed_word} with #{attempts_left} attempts left."
         end
-        
-        while(true)
-            input = gets
-            if(list_of_names.include?(input))
-                csv = CSV.read( "saved_games.csv", header: true )
-                row = csv.find {|row| row["name"] == input}
-                load_game(row)
-                break
-            else
-                puts "Could not find game with name #{input}, try again."
+
+        if(!list_of_names.empty?)
+            puts "Which of the above games do you want to load?"
+            while(true)
+                input = gets.chomp
+                if(list_of_names.include?(input))
+                    csv = CSV.read("saved_games.csv", headers: true, header_converters: :symbol)
+                    row = csv.find {|row| row[:name] == input}
+                    load_game(row)
+                    break
+                else
+                    puts "Could not find game with name #{input}, try again."
+                end
             end
+        else
+            puts "No available games to load..."
+            start_game
         end
     end
 
+    # Fix this method
     private def load_game(row)
-        puts row
-        puts "Successfully loaded game DWAKODAK"
+        @word = row[:word]
+        @guessed_word = row[:guessed_word]
+        @attempts_left = row[:attempts_left]
+        @wrongly_guessed = row[:wrongly_guessed]
+        puts "Successfully loaded game #{row[:name]}"
+        prompt_guesses
     end
 
     private def init_saved_game_folder
         if(!File.exist?("saved_games.csv"))
-            CSV.open("file.csv", "wb") do |csv|
+            CSV.open("saved_games.csv", "wb") do |csv|
                 csv << ["name", "word", "guessed_word", "attempts_left", "wrongly_guessed"]
             end
         end
@@ -116,14 +163,6 @@ class Hangman
         end
     end
 
-    def guess_char(char)
-        if(is_char?(char))
-            update_guess(char)
-        else
-            puts "Input is not a letter, try again."
-        end
-    end
-
     private def update_guess(char)
         contains_char = false
 
@@ -136,18 +175,14 @@ class Hangman
 
         if(contains_char)
             puts "#{char} is a correct guess!"
-            puts "Current progress: #{@guessed_word}"
-            puts "Attempts left: #{@attempts_left}"
             if(won?)
                 @game_over = true
-                print "You won with #{@attempts_left} attempts left. The word was #{@word}"
+                print "You won with #{@attempts_left} attempts left. The word was #{@word}\n"
             end
         else
             @attempts_left -= 1
-            @wrongly_guessed += [char]
+            @wrongly_guessed += char
             puts "#{char} is an incorrect guess!"
-            puts "Current progress: #{@guessed_word}"
-            puts "Attempts left: #{@attempts_left}"
             if(lost?)
                 @game_over = true
                 print "You lost. The word was #{@word}"
@@ -162,11 +197,6 @@ class Hangman
     private def lost?()
         return @attempts_left <= 0
     end
-
-    public def is_char?(char)
-        char.upcase != char.downcase && char.length == 1
-    end
-  
 end
 
 hm = Hangman.new()
